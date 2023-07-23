6694430f99f218840a598f724005d335bacb81e9
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2023-07-22 22:08:50 -0400

    crappy prototype ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Grid, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+
+const games = [
+	{
+		id: 1,
+		title: "Final Fantasy XVI"
+	},
+	{
+		id: 2,
+		title: "Baten Kaitos"
+	},
+	{
+		id: 3,
+		title: "Persona 3 Reload"
+	}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<>
+		<Typography variant='h1'>Omni-GLM Web Client Prototype</Typography>
+		<Table>
+			<TableHead>
+				<TableCell>ID</TableCell>
+				<TableCell>Title</TableCell>
+			</TableHead>
+			<TableBody>
+				{games.map((game) => (
+					<TableRow key={game.id}>
+						<TableCell>{game.id}</TableCell>
+						<TableCell>{game.title}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+		<Grid sm={6}>
+			<Button>Add Game</Button>
+			<Button>Edit Game</Button>
+			<Button>Delete Game</Button>
+			<Button>Search Games</Button>
+		</Grid>
+	</>
+);
 }
 
 export default App;
